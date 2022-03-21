cbd28d43098b9dad5ca8aa6edd8f02d172cacf6a
郭哲明 <jack@Jack-MacBook-Air.local>
2022-03-21 23:54:04 +0800

    use [type,subtype]for name

--- scripts/update_data.js
+++ scripts/update_data.js
@@ -7,7 +7,7 @@ socket.onopen = function(event) {
     console.log(event);
     setTimeout(function() {
         socket.send(JSON.stringify(
-            {name:"GPS",value:1.1,time:123.4}
+            {name:["FWS","L"],value:1.1,time:123.4}
         ));
     }, 1000);
 };
@@ -17,42 +17,69 @@ socket.onmessage = function(event) {
     console.log(event);
     var data = JSON.parse(event.data);
     console.log(data);
-    $("#"+data.name+"-value").text(data.value.toString());
+    $("#"+data.name[0]+"-"+data.name[1]+"-value").text(data.value.toString());
 };
 
 // data
-const data_name = {
-    FB1:{name:"FB1",value:1.0},
-    FB2:{name:"FB2",value:1.0},
-    RB1:{name:"RB1",value:1.0},
-    RB2:{name:"RB2",value:0.0},
-    DB1:{name:"DB1",value:0.0},
-    DB2:{name:"DB2",value:0.0},
-    MCM:{name:"MCM",value:0.0},
-    BMS:{name:"BMS",value:0.0},
-    HIS:{name:"HIS",value:0.0},
-    HIA:{name:"HIA",value:0.0},
-    HIG:{name:"HIG",value:0.0},
-    OIS:{name:"OIS",value:0.0},
-    OIA:{name:"OIA",value:0.0},
-    OIG:{name:"OIG",value:0.0},
-    OIC:{name:"OIC",value:0.0},
-    GPS:{name:"GPS",value:0.0}
-};
 
-function forEach(callback) {
-   for (let prop in this.container) {
-      // Call the callback as: callback(key, value)
-      callback(prop, this.container[prop]);
-   }
-}
+const data_list = [
+    {name:["FWS","L"],value:0.0},
+    {name:["FWS","R"],value:0.0},
+    {name:["FWT","L1"],value:0.0},
+    {name:["FWT","L2"],value:0.0},
+    {name:["FWT","R1"],value:0.0},
+    {name:["FWT","R2"],value:0.0},
+    {name:["BRK","N"],value:0.0},
+    {name:["THR","A"],value:0.0},
+    {name:["THR","B"],value:0.0},
+    {name:["STR","N"],value:0.0},
+    {name:["FSS","L"],value:0.0},
+    {name:["FSS","R"],value:0.0},
+    {name:["OPR","N"],value:0.0},
+    {name:["RWS","L"],value:0.0},
+    {name:["RWS","R"],value:0.0},
+    {name:["RWT","L1"],value:0.0},
+    {name:["RWT","L2"],value:0.0},
+    {name:["RWT","R1"],value:0.0},
+    {name:["RWT","R2"],value:0.0},
+    {name:["RSS","L"],value:0.0},
+    {name:["RSS","R"],value:0.0},
+    {name:["HIS","P"],value:0.0},
+    {name:["HIS","R"],value:0.0},
+    {name:["HIA","X"],value:0.0},
+    {name:["HIA","Y"],value:0.0},
+    {name:["HIA","Z"],value:0.0},
+    {name:["HIG","X"],value:0.0},
+    {name:["HIG","Y"],value:0.0},
+    {name:["HIG","Z"],value:0.0},
+    {name:["HIC","X"],value:0.0},
+    {name:["HIC","Y"],value:0.0},
+    {name:["HIC","Z"],value:0.0},
+    {name:["OIS","P"],value:0.0},
+    {name:["OIS","R"],value:0.0},
+    {name:["OIA","X"],value:0.0},
+    {name:["OIA","Y"],value:0.0},
+    {name:["OIA","Z"],value:0.0},
+    {name:["OIG","X"],value:0.0},
+    {name:["OIG","Y"],value:0.0},
+    {name:["OIG","Z"],value:0.0},
+    {name:["OIC","X"],value:0.0},
+    {name:["OIC","Y"],value:0.0},
+    {name:["OIC","Z"],value:0.0},
+    {name:["MTC","N"],value:0.0},
+    {name:["MSC","N"],value:0.0},
+    {name:["MDC","N"],value:0.0},
+    {name:["MIE","N"],value:0.0},
+    {name:["MID","N"],value:0.0},
+    {name:["MSM","N"],value:0.0},
+    {name:["MTL","N"],value:0.0}
+]
 
 
 $(document).ready(function(){ // or you can type "$(function(){}"
     // data_name.forEach((frame,content) => setupdata(frame,content));
-    for (const [frame, content] of Object.entries(data_name)) {
-        // $("#data-name").append($("<p></p>").attr("class","bg-body".attr("id",content.name)));
-        $("#data-name").append($("<p></p>").attr("class","bg-body").attr("id",content.name+"-name").text(content.name));
-        $("#data-value").append($("<p></p>").attr("class","bg-body").attr("id",content.name+"-value").text(content.value));
-    }
+    data_list.forEach(function(content, index){
+        $("#data-name").append($("<p></p>").attr("class","bg-body").attr("id",content.name[0]+"-"+content.name[1]+"-name").text(content.name));
+        $("#data-value").append($("<p></p>").attr("class","bg-body").attr("id",content.name[0]+"-"+content.name[1]+"-value").text(content.value));
+    });
 });
