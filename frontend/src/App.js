b816e0b209033516ea5425f8f7c2850a001ce93c
Chemmy-Jack <kuojack0114@gmail.com>
2022-12-04 22:06:33 +0800

    fix font

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import GUI from "./routes/GUI"
 import PlayGround from "./routes/PlayGround";
 // import SignIn from "./components/SigninExample"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import './App.css'
 
 function App() {
 	// let { WSmessage, sendData } = WebSocketState() ; 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { WebSocketProvider } from './routes/hooks/useWebSocket';
 import { FramesProvider } from './routes/hooks/useFrames';
-import styled from "styled-components"
-
-const StyledApp = styled(App)`
-    font-family: 'Noto Sans TC', 'Syncopate', 'xscale' ;font-size: 10em;
-`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	// <React.StrictMode>
 	<WebSocketProvider>
 		<FramesProvider>
-			<StyledApp />
+			<App />
 		</FramesProvider>
 	</WebSocketProvider>
 	// </React.StrictMode>
