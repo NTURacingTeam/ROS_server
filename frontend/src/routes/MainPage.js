bc07a56c539cfe1009ac936828f43df3af75fff1
liuyu <queeniiee02@gmail.com>
2022-12-04 21:17:24 +0800

    add img in homepage

--- frontend/src/routes/MainPage.js
+++ frontend/src/routes/MainPage.js
@@ -1,5 +1,10 @@
+import black from "../components/img/black.jpg"
+
 export default () => {
     return (
+        <>
         <h1>homePage</h1>
+        <img src={black}></img>
+        </>
     )
 }
