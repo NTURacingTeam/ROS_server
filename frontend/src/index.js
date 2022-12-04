998c0ec765b9ee3dbfa29fa8602da3302039849d
Chemmy-Jack <kuojack0114@gmail.com>
2022-12-04 21:45:46 +0800

    git add font family

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { WebSocketProvider } from './routes/hooks/useWebSocket';
 import { FramesProvider } from './routes/hooks/useFrames';
+import styled from "styled-components"
+
+const StyledApp = styled(App)`
+    font-family: 'Noto Sans TC', 'Syncopate', 'xscale' ;font-size: 10em;
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	// <React.StrictMode>
 	<WebSocketProvider>
 		<FramesProvider>
-			<App />
+			<StyledApp />
 		</FramesProvider>
 	</WebSocketProvider>
 	// </React.StrictMode>
