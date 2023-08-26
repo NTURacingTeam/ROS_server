ec7cebdb5c29e97da1550d06ee0f5942dee093fc
Chemmy-Jack <kuojack0114@gmail.com>
2023-08-27 01:44:13 +0800

    add last modify time of files

--- backend/records/index.php
+++ backend/records/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
  
 <head>
+		<title>NTURT Record Files</title>
     <style>
         body {font-size:200%;}
     </style>
@@ -8,14 +9,22 @@
  
 <body>
     <br>
- 
+<a target="_blank" rel="noopener noreferrer" href="https://www.csvplot.com/">Plot the csv here</a>
+    <br>
+    <br>
+
     <?php
     $i = 1;
     $g = glob("*");
     foreach ($g as $f)
     {
         if ($f == "index.php") continue;
-        echo $i . ". " . "<a href=\"" . $f . "\">" . $f . "</a><br><br>";
+        echo $i . ". " . "<a href=\"" . $f . "\">" . $f . "</a>  ";
+				$create = filectime($f);
+				$modify = filemtime($f);	
+				$modifyDate = date("y-m-d H:i:s", filemtime($f));
+				echo $modifyDate;
+				echo "<br><br>";
         $i = $i + 1;
     }
     ?>
