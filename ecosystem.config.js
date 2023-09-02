a90f48506fd8a3a3e1854b4506ef7011954a4877
Chemmy-Jack <kuojack0114@gmail.com>
2023-09-02 17:31:36 +0800

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,18 @@
 module.exports = {
   apps : [{
     name   : "ROS_server-frontend",
-    script : "cd frontend && pnpm start"
+    script : "cd frontend && pnpm start",
+		watch  : true,
   },
   {
     name   : "ROS_server-backend",
     script : "cd backend && pnpm start"
+		watch  : true,
   },
   {
     name   : "ROS_server-records",
     script : "cd backend/records && php -S 0.0.0.0:8889"
+		watch  : true,
   },
   ]
 }
