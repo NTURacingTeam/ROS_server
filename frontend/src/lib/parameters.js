010ff758d66be7b3d0f1018df79e97267642889d
Chemmy-Jack <kuojack0114@gmail.com>
2023-09-20 21:08:32 +0800

    change url params

--- frontend/src/lib/parameters.js
+++ frontend/src/lib/parameters.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "ws://140.112.222.153:21543"
-export const BACKEND_URL_HTTP = "http://140.112.222.153:21544"
+export const BACKEND_URL = "ws://140.112.14.14:21543"
+export const BACKEND_URL_HTTP = "http://140.112.14.14:21544"
